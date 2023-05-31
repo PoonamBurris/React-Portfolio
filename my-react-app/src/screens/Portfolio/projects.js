f711b3832a9f112d3d26a851664c12d8af6bcf05
PoonamBurris <119805763+PoonamBurris@users.noreply.github.com>
2023-05-30 20:53:52 -0400

    0.4.2
    Created Portfolio page

---
+++ my-react-app/src/screens/Portfolio/projects.js
@@ -0,0 +1,9 @@
+export const projects = [
+  {
+    title: "KPI",
+    description: "Key performance indicator",
+    image:"https://picsum.photos/900",
+    url: "https://picsum.photos/900",
+  },
+  
+]
--- my-react-app/src/screens/index.js
+++ my-react-app/src/screens/index.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar/Navbar";
 import Landing from "./Landing/Landing";
 import About from "./About/About";
+import Portfolio from "./Portfolio/Portfolio";
 
-export { Navbar, Landing, About };
+export { Navbar, Landing, About, Portfolio };
